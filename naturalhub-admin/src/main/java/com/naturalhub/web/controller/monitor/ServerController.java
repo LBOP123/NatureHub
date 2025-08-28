130cbf51ea8a8509e2b4f750e254ec7d95ec88a7
m0_73955675 <m0_73955675@noreply.gitcode.com>
2025-08-28 16:34:54 +0800

    init

---
+++ naturalhub-admin/src/main/java/com/naturalhub/web/controller/monitor/ServerController.java
@@ -0,0 +1,27 @@
+package com.naturalhub.web.controller.monitor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.naturalhub.common.core.domain.AjaxResult;
+import com.naturalhub.framework.web.domain.Server;
+
+/**
+ * 服务器监控
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
